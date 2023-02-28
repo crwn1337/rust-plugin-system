e1433ec59c0ba9435abf63d789e797f0f3394e8d
crwn1337 <38888474+crwn1337@users.noreply.github.com>
2023-02-28 23:34:45 +0200

    oops

--- plugin_example/src/lib.rs
+++ plugin_example/src/lib.rs
@@ -1,17 +1,17 @@
 include!("../../definitions/src/lib.rs");
 
-pub fn contains_hello(example: &dyn IExample) -> bool {
-    example.print();
+pub fn example_callback(example_struct: &dyn IExample) -> bool {
+    example_struct.print();
     true
 }
 
 pub fn init(interface: &mut dyn IInterface) {
-    interface.add_callback(contains_hello);
+    interface.add_callback(example_callback);
     println!("added callback(s) from the dll!");
 }
 
 pub fn shutdown(interface: &mut dyn IInterface) {
-    interface.remove_callback(contains_hello);
+    interface.remove_callback(example_callback);
     println!("removed callback(s) from the dll!");
 }
 
