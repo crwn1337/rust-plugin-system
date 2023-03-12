8871b5bc62d3c4ef1bc9f52d83b7515699a08dd6
crwn1337 <38888474+crwn1337@users.noreply.github.com>
2023-03-12 13:02:25 +0200

    macro hell

--- definitions/src/interface.rs
+++ definitions/src/interface.rs
@@ -1,26 +1,46 @@
+type Callback = fn(&mut dyn crate::IExample) -> bool;
+
 #[repr(C)]
 #[derive(Default)]
 pub struct Interface {
-    m_callbacks: Vec<fn(&mut dyn crate::IExample) -> bool>,
+    m_callbacks: Vec<Callback>,
 }
 
 pub trait IInterface {
-    fn add_callback(&mut self, callback: fn(&mut dyn crate::IExample) -> bool);
-    fn remove_callback(&mut self, callback: fn(&mut dyn crate::IExample) -> bool);
-    fn get_callbacks(&self) -> &Vec<fn(&mut dyn crate::IExample) -> bool>;
+    fn add_callback(&mut self, callback: Callback);
+    fn remove_callback(&mut self, callback: Callback);
+    fn get_callbacks(&self) -> &[Callback];
 }
 
 impl IInterface for Interface {
-    fn add_callback(&mut self, callback: fn(&mut dyn crate::IExample) -> bool) {
+    fn add_callback(&mut self, callback: Callback) {
         self.m_callbacks.push(callback);
     }
 
-    fn remove_callback(&mut self, callback: fn(&mut dyn crate::IExample) -> bool) {
+    fn remove_callback(&mut self, callback: Callback) {
         self.m_callbacks
             .retain(|c| *c as *const () != callback as *const ());
     }
 
-    fn get_callbacks(&self) -> &Vec<fn(&mut dyn crate::IExample) -> bool> {
+    fn get_callbacks(&self) -> &[Callback] {
         &self.m_callbacks
     }
+}
+
+#[macro_export]
+macro_rules! interface {
+    ($name:ident, $example_struct:ident, $($body:tt)*) => {
+        fn $name($example_struct: &mut dyn $crate::IInterface) {
+            $($body)*
+        }
+    };
+}
+
+#[macro_export]
+macro_rules! callback {
+    ($name:ident, $example_struct:ident, $($body:tt)*) => {
+        fn $name($example_struct: &mut dyn $crate::IExample) -> bool {
+            $($body)*
+        }
+    };
 }
--- plugin_example/src/lib.rs
+++ plugin_example/src/lib.rs
@@ -1,20 +1,20 @@
 include!("../../definitions/src/lib.rs");
 
-pub fn example_callback(example_struct: &mut dyn IExample) -> bool {
+callback!(example_callback, example_struct, {
     example_struct.m_age = 23;
     example_struct.print();
     true
-}
+});
 
-pub fn init(interface: &mut dyn IInterface) {
+interface!(init, interface, {
     interface.add_callback(example_callback);
     println!("added callback(s) from the dll!");
-}
+});
 
-pub fn shutdown(interface: &mut dyn IInterface) {
+interface!(shutdown, interface, {
     interface.remove_callback(example_callback);
     println!("removed callback(s) from the dll!");
-}
+});
 
 declare_plugin!(
     "plugin_example",
